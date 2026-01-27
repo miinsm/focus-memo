8d76aca6ca77ee37bbc2365a39111f5abc9c1aea
유가영 <gyullby11@gmail.com>
2026-01-27 20:20:45 +0900

    refactor: Redirect memo create to memo list by 유가영

--- memo/views.py
+++ memo/views.py
@@ -1,22 +1,20 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import MemoForm
 from .models import Memo
 from django.contrib import messages
 
 @login_required  # 로그인한 사용자만 접근 가능
+@login_required
 def memo_create(request):
     if request.method == 'POST':
         form = MemoForm(request.POST)
         if form.is_valid():
             memo = form.save(commit=False)
-            # 현재 로그인한 유저를 작성자로 지정
             memo.author = request.user
             memo.save()
             messages.success(request, '메모가 저장되었습니다!')
-            return redirect('/admin/')  # 임시로 관리자 페이지로
-        else:
-            messages.error(request, '입력 내용을 확인해 주세요.')
+            return redirect('memo_list')  # ✅ 수정 포인트
     else:
         form = MemoForm()
 
